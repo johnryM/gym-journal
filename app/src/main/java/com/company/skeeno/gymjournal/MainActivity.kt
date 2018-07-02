5583fbbb392c7ef1f7ba7caa5e7447b663e98262
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2018-07-02 23:42:51 +0100

    set up UI skeleton and use Navigation architecture component for managing it

--- app/src/main/java/com/company/skeeno/gymjournal/MainActivity.kt
+++ app/src/main/java/com/company/skeeno/gymjournal/MainActivity.kt
@@ -2,11 +2,27 @@ package com.company.skeeno.gymjournal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.ActionBar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(this, R.id.my_nav_host_fragment)
+        toolbar = supportActionBar!!
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        bottomNavigation.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
