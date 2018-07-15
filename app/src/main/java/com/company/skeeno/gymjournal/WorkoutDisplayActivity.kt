33586f90c7edc4bc33c666d581cc183f64cebb7a
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2018-07-16 00:23:04 +0100

    more work on the workout display page

--- app/src/main/java/com/company/skeeno/gymjournal/WorkoutDisplayActivity.kt
+++ app/src/main/java/com/company/skeeno/gymjournal/WorkoutDisplayActivity.kt
@@ -2,14 +2,39 @@ package com.company.skeeno.gymjournal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class WorkoutDisplayActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout_display)
 
+        navController = Navigation.findNavController(this, R.id.details_host_fragment)
+
         setSupportActionBar(findViewById(R.id.editor_toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.workout_details_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            R.id.save_menu_item -> {
+                Toast.makeText(this,"Save workout",Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
