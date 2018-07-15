08beb831608f7ff24e5f57966736c278f5e03479
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2018-07-15 17:35:14 +0100

    get editor activity set up

---
+++ app/src/main/java/com/company/skeeno/gymjournal/WorkoutDisplayActivity.kt
@@ -0,0 +1,12 @@
+package com.company.skeeno.gymjournal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutDisplayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_display)
+    }
+}
--- app/src/main/java/com/company/skeeno/gymjournal/WorkoutListFragment.kt
+++ app/src/main/java/com/company/skeeno/gymjournal/WorkoutListFragment.kt
@@ -1,12 +1,13 @@
 package com.company.skeeno.gymjournal
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_workout_list.*
 
 
 class WorkoutListFragment : Fragment() {
@@ -17,15 +18,11 @@ class WorkoutListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_list, container, false)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
-    override fun onDetach() {
-        super.onDetach()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fab.setOnClickListener({Navigation.findNavController(view).navigate(R.id.workoutDisplayActivity)})
+        super.onViewCreated(view, savedInstanceState)
     }
 }
